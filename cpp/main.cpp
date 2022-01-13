98a03a0fbe99c24dce6823e6a92abe794711c146
dufy <813540660@qq.com>
2022-01-13 23:18:57 +0800

    配置参考

---
+++ cpp/main.cpp
@@ -0,0 +1,22 @@
+/*
+参考：
+1. 《VS Code 搭建 C++ 开发环境（Mac 环境）》
+https://blog.csdn.net/qq_35787138/article/details/108830406
+2. <VSCode 右上角Run Code小三角图标消失问题>
+https://blog.csdn.net/Fox_Alex/article/details/106165959
+尤其是关于在终端显示的设置
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello world!" << endl;
+    cout << "hello c++" << endl;
+
+
+
+    return 0;
+}
